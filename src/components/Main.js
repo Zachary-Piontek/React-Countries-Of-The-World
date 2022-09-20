1095e9e580a39da0a2cd381c9c45b9e1dca0fc33
Zachary Piontek <zakpiontek@gmail.com>
2022-09-20 16:03:37 -0700

    added styles and changed image component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,29 +1,35 @@
 import React from 'react';
 import useCountry from '../hooks/useCountries';
 import CountryDisplay from './useCountries';
+import './main.css';
 
 export default function Main() {
   const { filterCountries, area, setArea } = useCountry();
 
   return ( 
     <>
-      <select value={area} onChange={(event) => {
-        setArea(event.target.value);
-      }}>
-        <option value='all'>All</option>
-        <option value='Africa'>Africa</option>
-        <option value='Antarctica'>Antarctica</option>
-        <option value='Asia'>Asia</option>
-        <option value='Oceania'>Oceania</option>
-        <option value="Europe">Europe</option>
-        <option value='North America'>North America</option>
-        <option value='South America'>South America</option>
-      </select>
-      <section>
-        {filterCountries().map((country) => (
-          <CountryDisplay {...country} key={country.id} />
-        ))}
-      </section>
+      <div className='main-page'>
+        <div className='header'>
+          <h1>Countries of the World</h1>
+        </div>
+        <select value={area} onChange={(event) => {
+          setArea(event.target.value);
+        }}>
+          <option value='all'>All</option>
+          <option value='Africa'>Africa</option>
+          <option value='Antarctica'>Antarctica</option>
+          <option value='Asia'>Asia</option>
+          <option value='Oceania'>Oceania</option>
+          <option value="Europe">Europe</option>
+          <option value='North America'>North America</option>
+          <option value='South America'>South America</option>
+        </select>
+        <section className='display-card'>
+          {filterCountries().map((country) => (
+            <CountryDisplay {...country} key={country.id} />
+          ))}
+        </section>
+      </div>
     </>
   );
 }
--- src/components/useCountries.js
+++ src/components/useCountries.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './useCountries.css';
 
 export default function CountryDisplay({ name, iso2 }) {
   return (
-    <div>
+    <div className='country-display'>
       <h3>{name}</h3>
-      <img src={`https://flagcdn.com/16x12/${iso2.toLowerCase()}.png`} />
+      <img src={`https://flagcdn.com/48x36/${iso2.toLowerCase()}.png`} />
     </div>
   );
 }
