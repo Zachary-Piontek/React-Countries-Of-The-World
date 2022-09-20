7e1b1da195ed8d9f7bd5e61fa6e5f2e2ec945465
Zachary Piontek <zakpiontek@gmail.com>
2022-09-20 13:06:09 -0700

    added select and options to main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,13 +3,27 @@ import useCountry from '../hooks/useCountries';
 import CountryDisplay from './useCountries';
 
 export default function Main() {
-  const { filterCountries } = useCountry();
+  const { filterCountries, area, setArea } = useCountry();
 
   return ( 
-    <section>
-      {filterCountries().map((country) => (
-        <CountryDisplay {...country} key={country.id} />
-      ))}
-    </section>
+    <>
+      <select value={area} onChange={(event) => {
+        setArea(event.target.value);
+      }}>
+        <option value='all'>All</option>
+        <option value='Africa'>Africa</option>
+        <option value='Antarctica'>Antarctica</option>
+        <option value='Asia'>Asia</option>
+        <option value='Austrailia'>Austrailia</option>
+        <option value="Europe">Europe</option>
+        <option value='North America'>North America</option>
+        <option value='South America'>South America</option>
+      </select>
+      <section>
+        {filterCountries().map((country) => (
+          <CountryDisplay {...country} key={country.id} />
+        ))}
+      </section>
+    </>
   );
 }
