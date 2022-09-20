1c40df820ed1c5151e722eb44a06d0bd6a3a5981
Zachary Piontek <zakpiontek@gmail.com>
2022-09-20 14:40:42 -0700

    images display for countries

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,7 +14,7 @@ export default function Main() {
         <option value='Africa'>Africa</option>
         <option value='Antarctica'>Antarctica</option>
         <option value='Asia'>Asia</option>
-        <option value='Austrailia'>Austrailia</option>
+        <option value='Oceania'>Oceania</option>
         <option value="Europe">Europe</option>
         <option value='North America'>North America</option>
         <option value='South America'>South America</option>
--- src/components/useCountries.js
+++ src/components/useCountries.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-export default function CountryDisplay({ id, name }) {
+export default function CountryDisplay({ name, iso2 }) {
   return (
     <div>
-      <h3>{name} {id}</h3>
+      <h3>{name}</h3>
+      <img src={`https://flagcdn.com/16x12/${iso2.toLowerCase()}.png`} />
     </div>
   );
 }
